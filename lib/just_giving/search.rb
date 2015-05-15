4fe7abea71d34162fd6a2915d1cead5d60754af5
John Polling <john@theled.co.uk>
2015-05-15 16:22:49 +0100

    Defaults
    Adding in some defaults

--- lib/just_giving/search.rb
+++ lib/just_giving/search.rb
@@ -11,7 +11,7 @@ module JustGiving
     end
 
     # Search fundraising
-    def search_fundraising(query, charity_id, cause_id, event_id, design_id, status, event_date_range, page, per_page)
+    def search_fundraising(query, charity_id=nil, cause_id=nil, event_id=nil, design_id=nil, status=nil, event_date_range=nil, page=1, per_page=10)
       get("v1/fundraising/search?q=#{query}&=page=#{page}&pagesize=#{per_page}&causeid=#{cause_id}&eventid=#{event_id}&charityid=#{charity_id}&designid=#{design_id}&status=#{status}&eventdaterange=#{event_date_range}")
     end
   end
