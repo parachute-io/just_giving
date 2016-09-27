63d0675a9c9bf20ec2b967acbf84284204a1ef78
Richard Worrall <richard@skinnyfit.com>
2016-09-27 17:21:42 +0100

    Tidy up test helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,18 +1,7 @@
-require 'rubygems'
-require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'test/unit'
 require 'webmock/test_unit'
 require 'shoulda'
-
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'just_giving'
 
 def stub_get(path, basic_auth=nil)
